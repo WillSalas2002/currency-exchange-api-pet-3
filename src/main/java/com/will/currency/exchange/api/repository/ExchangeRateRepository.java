d21fd502dd4de42b34c7bfd5ccbfb7af03fdef3f
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-06 20:52:49 +0500

    feat: create ExchangeRateRepository class for CRUD manipulation of ExchangeRate entity in db

---
+++ src/main/java/com/will/currency/exchange/api/repository/ExchangeRateRepository.java
@@ -0,0 +1,14 @@
+package com.will.currency.exchange.api.repository;
+
+import com.will.currency.exchange.api.model.Currency;
+import com.will.currency.exchange.api.model.ExchangeRate;
+import com.will.currency.exchange.api.util.ConnectionManager;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ExchangeRateRepository {
+
+}
