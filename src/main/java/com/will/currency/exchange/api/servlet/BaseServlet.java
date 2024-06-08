e52f9a3d73d8469792ee964f62bd031019a5acf2
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-08 21:06:11 +0500

    feat(servlet): create base servlet mainly for implementing centralized exception handling logic

---
+++ src/main/java/com/will/currency/exchange/api/servlet/BaseServlet.java
@@ -0,0 +1,20 @@
+package com.will.currency.exchange.api.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public abstract class BaseServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
