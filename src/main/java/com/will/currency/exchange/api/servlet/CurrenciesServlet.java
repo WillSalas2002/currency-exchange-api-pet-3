8510d0f77387341a848a3c7fcdc80dfbef0fd737
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-08 21:24:41 +0500

    feat(servlet): implement getting all currencies.

--- src/main/java/com/will/currency/exchange/api/servlet/CurrenciesServlet.java
+++ src/main/java/com/will/currency/exchange/api/servlet/CurrenciesServlet.java
@@ -1,16 +1,23 @@
 package com.will.currency.exchange.api.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.will.currency.exchange.api.response.CurrencyResponse;
+import com.will.currency.exchange.api.service.CurrencyService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 public class CurrenciesServlet extends BaseServlet {
+    private final CurrencyService currencyService = new CurrencyService();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        List<CurrencyResponse> currencies = currencyService.findAll();
+        objectMapper.writeValue(resp.getWriter(), currencies);
     }
 
     @Override
