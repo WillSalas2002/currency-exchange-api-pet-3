adcc567f3051709c30ce8ad8e3f47d5f3b92bac5
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-08 21:06:55 +0500

    feat(servlet): create CurrencyServlet

---
+++ src/main/java/com/will/currency/exchange/api/servlet/CurrencyServlet.java
@@ -0,0 +1,22 @@
+package com.will.currency.exchange.api.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/currencies")
+public class CurrencyServlet extends BaseServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
