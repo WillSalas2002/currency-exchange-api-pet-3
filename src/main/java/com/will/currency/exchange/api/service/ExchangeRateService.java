86dc6db771832d67b7b4cd190f91d4234abda16e
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-09 18:22:40 +0500

    feat(service): implement findAll method

--- src/main/java/com/will/currency/exchange/api/service/ExchangeRateService.java
+++ src/main/java/com/will/currency/exchange/api/service/ExchangeRateService.java
@@ -1,8 +1,28 @@
 package com.will.currency.exchange.api.service;
 
+import com.will.currency.exchange.api.model.ExchangeRate;
 import com.will.currency.exchange.api.repository.ExchangeRateRepository;
+import com.will.currency.exchange.api.response.ExchangeRateResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ExchangeRateService {
-    private final ExchangeRateRepository exchangeRateRepository = new ExchangeRateRepository();
+    private final ExchangeRateRepository repository = new ExchangeRateRepository();
 
+    public List<ExchangeRateResponse> findAll() {
+        return repository.findAll()
+                .stream()
+                .map(this::convertToExchangeRateResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ExchangeRateResponse convertToExchangeRateResponse(ExchangeRate exchangeRate) {
+        return new ExchangeRateResponse(
+                exchangeRate.getId(),
+                exchangeRate.getBaseCurrency(),
+                exchangeRate.getTargetCurrency(),
+                exchangeRate.getRate()
+        );
+    }
 }
