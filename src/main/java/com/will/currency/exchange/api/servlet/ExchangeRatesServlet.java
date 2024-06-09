95ebccfc480e104b4ff94ae09ce710b37e63e348
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-09 18:25:27 +0500

    feat(servlet): implement doGet() method

--- src/main/java/com/will/currency/exchange/api/servlet/ExchangeRatesServlet.java
+++ src/main/java/com/will/currency/exchange/api/servlet/ExchangeRatesServlet.java
@@ -1,18 +1,26 @@
 package com.will.currency.exchange.api.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.will.currency.exchange.api.response.ExchangeRateResponse;
+import com.will.currency.exchange.api.service.ExchangeRateService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/exchangeRates")
 public class ExchangeRatesServlet extends BaseServlet {
 
+    private final ExchangeRateService exchangeRateService = new ExchangeRateService();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        List<ExchangeRateResponse> exchangeRates = exchangeRateService.findAll();
+        objectMapper.writeValue(resp.getWriter(), exchangeRates);
     }
 
     @Override
