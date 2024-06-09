185c6f886095ebddc590ea740ae9d30e070a5440
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-09 18:35:12 +0500

    refactor(dto): change them from record to class for more readability and add nessecary constructors

--- src/main/java/com/will/currency/exchange/api/response/CurrencyResponse.java
+++ src/main/java/com/will/currency/exchange/api/response/CurrencyResponse.java
@@ -1,9 +1,20 @@
 package com.will.currency.exchange.api.response;
 
-public record CurrencyResponse(
-        int id,
-        String code,
-        String fullName,
-        String sign
-) {
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CurrencyResponse {
+    private int id;
+    private String code;
+    private String fullName;
+    private String sign;
+
+    public CurrencyResponse(String code, String fullName, String sign) {
+        this.code = code;
+        this.fullName = fullName;
+        this.sign = sign;
+    }
 }
--- src/main/java/com/will/currency/exchange/api/response/ExchangeRateResponse.java
+++ src/main/java/com/will/currency/exchange/api/response/ExchangeRateResponse.java
@@ -1,13 +1,26 @@
 package com.will.currency.exchange.api.response;
 
 import com.will.currency.exchange.api.model.Currency;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 
-public record ExchangeRateResponse(
-        int id,
-        Currency baseCurrency,
-        Currency targetCurrency,
-        BigDecimal rate
-) {
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ExchangeRateResponse {
+    private int id;
+    private Currency baseCurrency;
+    private Currency targetCurrency;
+    private BigDecimal rate;
+
+    public ExchangeRateResponse(Currency baseCurrency, Currency targetCurrency, BigDecimal rate) {
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate;
+    }
 }
