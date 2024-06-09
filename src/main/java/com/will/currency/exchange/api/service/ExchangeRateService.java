d2232eec87193c2f08c0b4e24947dca9d4994a25
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-09 18:16:56 +0500

    feat(service): create ExchangeRateService

---
+++ src/main/java/com/will/currency/exchange/api/service/ExchangeRateService.java
@@ -0,0 +1,8 @@
+package com.will.currency.exchange.api.service;
+
+import com.will.currency.exchange.api.repository.ExchangeRateRepository;
+
+public class ExchangeRateService {
+    private final ExchangeRateRepository exchangeRateRepository = new ExchangeRateRepository();
+
+}
