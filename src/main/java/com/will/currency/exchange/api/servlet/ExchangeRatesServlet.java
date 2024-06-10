a2f2716d173882461d3779b928dfbc27f9cb4161
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-10 19:20:33 +0500

    feat(servlet): implement doPost method in ExchangeRatesServlet

--- src/main/java/com/will/currency/exchange/api/servlet/ExchangeRatesServlet.java
+++ src/main/java/com/will/currency/exchange/api/servlet/ExchangeRatesServlet.java
@@ -1,7 +1,9 @@
 package com.will.currency.exchange.api.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.will.currency.exchange.api.response.CurrencyResponse;
 import com.will.currency.exchange.api.response.ExchangeRateResponse;
+import com.will.currency.exchange.api.service.CurrencyService;
 import com.will.currency.exchange.api.service.ExchangeRateService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,12 +11,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 
 @WebServlet("/exchangeRates")
 public class ExchangeRatesServlet extends BaseServlet {
 
     private final ExchangeRateService exchangeRateService = new ExchangeRateService();
+    private final CurrencyService currencyService = new CurrencyService();
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
@@ -25,6 +29,20 @@ public class ExchangeRatesServlet extends BaseServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String baseCurrencyCode = req.getParameter("baseCurrencyCode");
+        String targetCurrencyCode = req.getParameter("targetCurrencyCode");
+        String rateStr = req.getParameter("rate");
+
+        // TODO: need to add validation of parameters
+
+        baseCurrencyCode = baseCurrencyCode.toUpperCase();
+        targetCurrencyCode = targetCurrencyCode.toUpperCase();
+        BigDecimal rate = new BigDecimal(rateStr);
+        CurrencyResponse baseCurrency = currencyService.findByCurrencyCode(baseCurrencyCode);
+        CurrencyResponse targetCurrency = currencyService.findByCurrencyCode(targetCurrencyCode);
+        ExchangeRateResponse exchangeRate = new ExchangeRateResponse(baseCurrency, targetCurrency, rate);
+        ExchangeRateResponse savedExchangeRate = exchangeRateService.save(exchangeRate);
+
+        objectMapper.writeValue(resp.getWriter(), savedExchangeRate);
     }
 }
