08284c08dfc36a8f4252d6ea1104383c918fc9bd
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-10 19:19:37 +0500

    feat(servlet): implement doPost method in CurrenciesServlet

--- src/main/java/com/will/currency/exchange/api/servlet/CurrenciesServlet.java
+++ src/main/java/com/will/currency/exchange/api/servlet/CurrenciesServlet.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.will.currency.exchange.api.response.CurrencyResponse;
 import com.will.currency.exchange.api.service.CurrencyService;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.List;
 
+@WebServlet("/currencies")
 public class CurrenciesServlet extends BaseServlet {
     private final CurrencyService currencyService = new CurrencyService();
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -22,6 +24,13 @@ public class CurrenciesServlet extends BaseServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String code = req.getParameter("code");
+        String fullName = req.getParameter("fullName");
+        String sign = req.getParameter("sign");
+        // TODO: need to add validation
+
+        CurrencyResponse currency = new CurrencyResponse(0, code, fullName, sign);
+        CurrencyResponse currencyResponse = currencyService.save(currency);
+        objectMapper.writeValue(resp.getWriter(), currencyResponse);
     }
 }
