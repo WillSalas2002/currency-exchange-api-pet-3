0a8015e1cec3e7d9005fd992a6a81660a16a4c8c
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-10 19:19:15 +0500

    feat(servlet): implement doGet method in CurrencyServlet

--- src/main/java/com/will/currency/exchange/api/servlet/CurrencyServlet.java
+++ src/main/java/com/will/currency/exchange/api/servlet/CurrencyServlet.java
@@ -1,5 +1,8 @@
 package com.will.currency.exchange.api.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.will.currency.exchange.api.response.CurrencyResponse;
+import com.will.currency.exchange.api.service.CurrencyService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,16 +10,16 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/currencies")
+@WebServlet("/currency/*")
 public class CurrencyServlet extends BaseServlet {
+    private final CurrencyService currencyService = new CurrencyService();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String currencyCode = req.getPathInfo().replace("/", "").toUpperCase();
+        // TODO: need to add validation
+        CurrencyResponse currencyResponse = currencyService.findByCurrencyCode(currencyCode);
+        objectMapper.writeValue(resp.getWriter(), currencyResponse);
     }
 }
