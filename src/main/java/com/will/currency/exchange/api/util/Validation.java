439071f34633e9dcdaacec2108a56a52bc5ff44d
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-17 22:54:09 +0500

    feat(validation): add validation for rate

--- src/main/java/com/will/currency/exchange/api/util/Validation.java
+++ src/main/java/com/will/currency/exchange/api/util/Validation.java
@@ -1,5 +1,7 @@
 package com.will.currency.exchange.api.util;
 
+import java.util.regex.Pattern;
+
 public class Validation {
 
     // Method to validate currency code
@@ -15,6 +17,10 @@ public class Validation {
     // Method to validate sign
     public static boolean isValidSign(String sign) {
         return sign != null && sign.length() == 1;
+    }
+
+    public static boolean isValidRate(String rate) {
+        return rate != null && rate.matches("^\\d+(\\.\\d+)?$");
     }
 
     // Method to validate the whole currency object
