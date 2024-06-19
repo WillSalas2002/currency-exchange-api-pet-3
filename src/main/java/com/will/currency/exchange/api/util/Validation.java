e7dbc07c833b7c03cd1c17ae5dd33dab0eeac17a
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-06-19 21:36:40 +0500

    fix(validation): it is now case-insensitive while checking currency code

--- src/main/java/com/will/currency/exchange/api/util/Validation.java
+++ src/main/java/com/will/currency/exchange/api/util/Validation.java
@@ -1,20 +1,15 @@
 package com.will.currency.exchange.api.util;
 
-import java.util.regex.Pattern;
-
 public class Validation {
 
-    // Method to validate currency code
     public static boolean isValidCode(String code) {
-        return code != null && code.matches("[A-Z]{3}");
+        return code != null && code.matches("[a-zA-Z]{3}");
     }
 
-    // Method to validate full name
     public static boolean isValidFullName(String fullName) {
         return fullName != null && fullName.length() > 1 && fullName.length() <= 100;
     }
 
-    // Method to validate sign
     public static boolean isValidSign(String sign) {
         return sign != null && sign.length() == 1;
     }
@@ -23,7 +18,6 @@ public class Validation {
         return rate != null && rate.matches("^\\d+(\\.\\d+)?$");
     }
 
-    // Method to validate the whole currency object
     public static boolean isValidCurrency(String code, String fullName, String sign) {
         return isValidCode(code) && isValidFullName(fullName) && isValidSign(sign);
     }
