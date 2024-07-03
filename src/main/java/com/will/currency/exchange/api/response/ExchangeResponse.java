2d409daf181ded2d4ccac6c5b26de6d22fd835f9
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-07-03 20:07:00 +0500

    feat(dto): create ExchangeResponse object for representing exchanged entity

---
+++ src/main/java/com/will/currency/exchange/api/response/ExchangeResponse.java
@@ -0,0 +1,17 @@
+package com.will.currency.exchange.api.response;
+
+import com.will.currency.exchange.api.model.Currency;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Getter
+public class ExchangeResponse {
+    private Currency baseCurrency;
+    private Currency targetCurrency;
+    private BigDecimal rate;
+    private BigDecimal amount;
+    private BigDecimal convertedAmount;
+}
