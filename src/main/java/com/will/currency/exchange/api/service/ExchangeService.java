22f4645b05a2fa745ce86c933742a799148b3dd6
Will Salas <i.jumatov@infinbank.com>
2024-09-19 17:50:45 +0500

    chore(service): change rounding modes and rename methods logically

--- src/main/java/com/will/currency/exchange/api/service/ExchangeService.java
+++ src/main/java/com/will/currency/exchange/api/service/ExchangeService.java
@@ -13,21 +13,20 @@ import java.util.Optional;
 
 public class ExchangeService {
     private final ExchangeRateRepository exchangeRateRepository = new ExchangeRateRepository();
-    private final String USD_CODE = "USD";
 
     public Optional<ExchangeResponse> exchange(CurrencyResponse baseCurrency, CurrencyResponse targetCurrency, BigDecimal amount) throws SQLException {
 
-        Optional<ExchangeRate> directExchangeRateOptional = findDirectExchangeRate(baseCurrency, targetCurrency);
+        Optional<ExchangeRate> directExchangeRateOptional = buildDirectExchangeRate(baseCurrency, targetCurrency);
         if (directExchangeRateOptional.isPresent()) {
             return Optional.of(calculateExchangeAmount(directExchangeRateOptional.get(), amount));
         }
 
-        Optional<ExchangeRate> reversedExchangeRateOptional = findReversedExchangeRate(baseCurrency, targetCurrency);
+        Optional<ExchangeRate> reversedExchangeRateOptional = buildReversedExchangeRate(baseCurrency, targetCurrency);
         if (reversedExchangeRateOptional.isPresent()) {
             return Optional.of(calculateReversedExchangeAmount(reversedExchangeRateOptional.get(), amount));
         }
 
-        Optional<ExchangeRate> rateWithUSDBaseOptional = findExchangeRateUsingUSD(baseCurrency, targetCurrency);
+        Optional<ExchangeRate> rateWithUSDBaseOptional = buildExchangeRateUsingUSD(baseCurrency, targetCurrency);
         if (rateWithUSDBaseOptional.isPresent()) {
             return Optional.of(calculateExchangeAmount(rateWithUSDBaseOptional.get(), amount));
         }
@@ -35,15 +34,16 @@ public class ExchangeService {
         return Optional.empty();
     }
 
-    private Optional<ExchangeRate> findDirectExchangeRate(CurrencyResponse baseCurrency, CurrencyResponse targetCurrency) throws SQLException {
+    private Optional<ExchangeRate> buildDirectExchangeRate(CurrencyResponse baseCurrency, CurrencyResponse targetCurrency) throws SQLException {
         return exchangeRateRepository.findByCurrencyCodes(baseCurrency.getCode(), targetCurrency.getCode());
     }
 
-    private Optional<ExchangeRate> findReversedExchangeRate(CurrencyResponse baseCurrency, CurrencyResponse targetCurrency) throws SQLException {
+    private Optional<ExchangeRate> buildReversedExchangeRate(CurrencyResponse baseCurrency, CurrencyResponse targetCurrency) throws SQLException {
         return exchangeRateRepository.findByCurrencyCodes(targetCurrency.getCode(), baseCurrency.getCode());
     }
 
-    private Optional<ExchangeRate> findExchangeRateUsingUSD(CurrencyResponse baseCurrency, CurrencyResponse targetCurrency) throws SQLException {
+    private Optional<ExchangeRate> buildExchangeRateUsingUSD(CurrencyResponse baseCurrency, CurrencyResponse targetCurrency) throws SQLException {
+        final String USD_CODE = "USD";
         Optional<ExchangeRate> baseToUsdRate = exchangeRateRepository.findByCurrencyCodes(USD_CODE, baseCurrency.getCode());
         Optional<ExchangeRate> targetToUsdRate = exchangeRateRepository.findByCurrencyCodes(USD_CODE, targetCurrency.getCode());
         if (baseToUsdRate.isPresent() && targetToUsdRate.isPresent()) {
@@ -55,13 +55,13 @@ public class ExchangeService {
     }
 
     private ExchangeResponse calculateReversedExchangeAmount(ExchangeRate reversedExchangeRate, BigDecimal amount) {
-        BigDecimal reversedRate = BigDecimal.ONE.divide(reversedExchangeRate.getRate(), 2, RoundingMode.HALF_EVEN);
+        BigDecimal reversedRate = BigDecimal.ONE.divide(reversedExchangeRate.getRate(), 4, RoundingMode.HALF_EVEN);
         ExchangeRate exchangeRate = buildExchangeRate(reversedExchangeRate.getTargetCurrency(), reversedExchangeRate.getBaseCurrency(), reversedRate);
         return calculateExchangeAmount(exchangeRate, amount);
     }
 
     private ExchangeResponse calculateExchangeAmount(ExchangeRate exchangeRate, BigDecimal amount) {
-        BigDecimal convertedAmount = exchangeRate.getRate().multiply(amount);
+        BigDecimal convertedAmount = exchangeRate.getRate().multiply(amount).setScale(2, RoundingMode.HALF_EVEN);
         return new ExchangeResponse(exchangeRate.getBaseCurrency(), exchangeRate.getTargetCurrency(), exchangeRate.getRate(), amount, convertedAmount);
     }
 
