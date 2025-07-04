378aefcf92230645dc2510e15a6090eac03842a2
Ilkham Jumatov <batir2jumatov@gmail.com>
2025-07-04 16:54:47 +0500

    create new custom exception

---
+++ src/main/java/com/will/currency/exchange/api/exception/DatabaseInitializationException.java
@@ -0,0 +1,8 @@
+package com.will.currency.exchange.api.exception;
+
+public class DatabaseInitializationException extends RuntimeException {
+
+    public DatabaseInitializationException() {
+        super();
+    }
+}
